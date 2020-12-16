92e32798843814c13578263af4c575e93216d542
sanderjson <sanderson.jma@gmail.com>
2020-12-16 11:28:31 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Doodle from "./Doodle.svelte";
+  import Message from "./Message.svelte";
+
+  let cssDoodle = { update: () => {} };
+  $: doodle = `
+		:doodle {
+			@grid: 6 / 100vmax;
+			height: 200px;
+			width: 200px;
+			
+    }
+@shape: circle;
+		transform:
+				scale(${scale}) 
+				translate(@r(-5px, 5px), @r(-5px, 5px)) 
+				rotate(@r(-10deg, 10deg));
+		opacity: @r(.2, 1);
+		background: @pick(${color}, #fff);
+	`;
+
+  $: cssDoodle.update(doodle);
+
+  let color = "blueviolet";
+  let scale = "@rand(1,2)";
+</script>
+
+<style>
+  /* :global(css-doodle) {
+    margin: auto;
+  } */
+</style>
+
+<!-- <css-doodle bind:this={cssDoodle} /> -->
+
+<Doodle />
+<Message />
+
+<svelte:head>
+  <script defer src="https://unpkg.com/css-doodle@0.8.5/css-doodle.min.js">
+
+  </script>
+</svelte:head>
---
+++ src/Message.svelte
@@ -0,0 +1,64 @@
+<script>
+
+</script>
+
+<style>
+  .wrapper {
+    position: absolute;
+    display: grid;
+    place-content: center;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 999;
+  }
+
+  .bg {
+    display: flex;
+    background: white;
+    width: 240px;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem 3rem;
+    border-radius: 0.4em;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
+
+  .current-day {
+    font-size: 6rem;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    text-transform: uppercase;
+    width: 100%;
+  }
+  .current-num {
+    font-size: 12rem;
+    margin: 0;
+    padding: 0;
+    line-height: 0.618;
+    text-align: center;
+    text-transform: uppercase;
+    width: 100%;
+  }
+
+  .message-body {
+    padding-block-start: 4rem;
+  }
+</style>
+
+<div class="wrapper">
+  <div class="bg">
+    <section>
+      <h1 class="message-header">Hey Cassy!</h1>
+      <div class="current-day">day</div>
+      <div class="current-num">4</div>
+      <p class="message-body">
+        You said it takes 21 days to make or break a habit. On January 4, 2021
+        you will have quit smoking. We all know you can do anything you put your
+        mind to. Check back here tomorrow.
+      </p>
+    </section>
+  </div>
+</div>
