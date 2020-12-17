a8090065c815fdd4a94075698f45c19753cfda78
sanderjson <sanderson.jma@gmail.com>
2020-12-17 14:19:19 -0700

    make responsive fix date calc

--- src/Message.svelte
+++ src/Message.svelte
@@ -1,9 +1,17 @@
 <script>
   let quitStart = new Date(2020, 11, 12);
   let today = new Date();
+  let todayFixed = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate()
+  );
+
+  console.log(todayFixed);
   // let todayYear = today.getFullYear();
   // let todayMonth = today.getMonth() + 1;
-  let quitTime = today - quitStart;
+
+  let quitTime = todayFixed - quitStart;
   let quitDay = (quitTime / 1000 / 3600 / 24).toFixed(0);
 </script>
 
@@ -12,17 +20,19 @@
 </style>
 
 <div class="absolute inset-0 flex justify-center items-center">
-  <div class="container m-8 bg-white rounded-lg shadow-xl">
-    <section class="py-6 px-4">
+  <div class="container m-8 bg-white rounded-lg shadow-xl max-w-screen-sm transition-all duration-150">
+    <section class="py-6 px-4 sm:py-10 sm:px-8">
       <div
-        class="text-2xl text-gray-500 text-center uppercase leading-none m-0 p-0">
+        class="text-2xl sm:text-5xl text-gray-500 text-center uppercase
+        leading-none m-0 p-0">
         day
       </div>
-      <div class="text-9xl font-bold text-center leading-none m-0 p-0">
+      <div
+        class="text-9xl sm:text-12xl font-bold text-center leading-none m-0 p-0">
         {quitDay}
       </div>
-      <h1 class="text-base pt-3">Hey Cassy!</h1>
-      <p class="text-sm text-gray-500">
+      <h1 class="text-base sm:text-3xl font-bold pt-3">Hey Cassy!</h1>
+      <p class="text-sm sm:text-2xl text-gray-500 pt-1">
         You said it takes 21 days to make or break a habit. On January 4, 2021
         you will have quit smoking. We all know you can do anything you put your
         mind to.
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	theme: {
+		extend: {
+			fontSize: {
+				"10xl": "10.25rem",
+				"11xl": "12.75rem",
+				"12xl": "15.50rem",
+			},
+		},
+	},
+};
