17bbb9576ea8163c769a5eba03d40aec15137442
sanderjson <sanderson.jma@gmail.com>
2020-12-18 10:47:50 -0700

    minor css fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,13 @@
 <script>
   import Doodle from "./Doodle.svelte";
   import Message from "./Message.svelte";
-
-  let cssDoodle = { update: () => {} };
-  $: doodle = `
-		:doodle {
-			@grid: 6 / 100vmax;
-			height: 200px;
-			width: 200px;
-			
-    }
-@shape: circle;
-		transform:
-				scale(${scale}) 
-				translate(@r(-5px, 5px), @r(-5px, 5px)) 
-				rotate(@r(-10deg, 10deg));
-		opacity: @r(.2, 1);
-		background: @pick(${color}, #fff);
-	`;
-
-  $: cssDoodle.update(doodle);
-
-  let color = "blueviolet";
-  let scale = "@rand(1,2)";
 </script>
 
-<style global>
-  /* purgecss start ignore */
-  @tailwind base;
-  @tailwind components;
-  /* purgecss end ignore */
-  @tailwind utilities;
-</style>
 
-<!-- <css-doodle bind:this={cssDoodle} /> -->
+<div class="h-screen w-screen overflow-hidden">
+  <Doodle />
+</div>
 
-<Doodle />
 <Message />
 
 <svelte:head>
--- src/Message.svelte
+++ src/Message.svelte
@@ -20,11 +20,11 @@
 </style>
 
 <div class="absolute inset-0 flex justify-center items-center">
-  <div class="container m-8 bg-white rounded-lg shadow-xl max-w-screen-sm transition-all duration-150">
-    <section class="py-6 px-4 sm:py-10 sm:px-8">
+  <div class="container my-8 mx-16 bg-white rounded-lg shadow-xl max-w-screen-sm transition-all duration-150">
+    <section class="py-8 px-4 sm:py-10 sm:px-8">
       <div
         class="text-2xl sm:text-5xl text-gray-500 text-center uppercase
-        leading-none m-0 p-0">
+        leading-none">
         day
       </div>
       <div
@@ -32,7 +32,7 @@
         {quitDay}
       </div>
       <h1 class="text-base sm:text-3xl font-bold pt-3">Hey Cassy!</h1>
-      <p class="text-sm sm:text-2xl text-gray-500 pt-1">
+      <p class="text-sm sm:text-2xl text-gray-500 pt-1 sm:pt-2">
         You said it takes 21 days to make or break a habit. On January 4, 2021
         you will have quit smoking. We all know you can do anything you put your
         mind to.
